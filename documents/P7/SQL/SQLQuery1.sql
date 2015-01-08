03355e16e7f38ec97b2e7102acf44a238c2abb2a
dym_ASUS <308960474@qq.com>
2015-01-08 21:11:41 +0800

    add db for chapter7

--- documents/P7/SQL/SQLQuery1.sql
+++ documents/P7/SQL/SQLQuery1.sql
@@ -3,3 +3,11 @@ select * from tb_taoti
 Select * From kecheng_taoti_view ORDER BY Id DESC
 
 select * from tb_Lesson
+
+select * from tb_Profession
+
+select * from tb_Lesson where ofprofession=
+
+select a.*,b.ofProfession from tb_TaoTi as a join tb_Lesson as b on a.LessonID=b.ID where a.LessonID= and b.ofProfession=
+
+select * from tb_questions
